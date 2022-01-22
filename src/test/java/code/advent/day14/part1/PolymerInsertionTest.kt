a02a0373cecc17d20670886de0a42d10641e80a6
Brendan <brendan.hatton@gmail.com>
2022-01-22 23:33:59 +1100

    Day 14 part 2

--- src/test/java/code/advent/day14/part1/PolymerInsertionTest.kt
+++ src/test/java/code/advent/day14/part1/PolymerInsertionTest.kt
@@ -4,9 +4,7 @@ import org.junit.jupiter.api.Test
 import java.io.File
 import java.net.URISyntaxException
 import java.util.*
-import javax.naming.ldap.PagedResultsControl
 import kotlin.collections.HashMap
-import kotlin.test.assertEquals
 
 internal class PolymerInsertionTest {
 
@@ -35,36 +33,72 @@ internal class PolymerInsertionTest {
             val polymer = parsedInputs.first
             val rules = parsedInputs.second
 //            assertEquals(expectedResults[0], polymer)
-            var updated = polymer
-            for (i in 1..10) {
-                updated = updated.mapIndexed { index, c ->
-                    if (index < updated.length - 1) {
-                        val polymerPair = "$c${updated[index + 1]}"
-                        val toInsert = rules[polymerPair]
-                        "$c${toInsert?.let { toInsert }}"
-                    } else {
-                        "$c"
-                    }
-
-                }.joinToString("")
-                println(updated)
-//                assertEquals(expectedResults[i], updated)
-            }
-
-            val count = HashMap<Char, Int>()
-            updated.forEach { c ->
-                count[c] = if (count[c] == null) {
-                    1
-                } else {
-                    count[c]!! + 1
+            val tokenCountMap = HashMap<String, Long>()
+            polymer.mapIndexed { index, c ->
+                if (index < polymer.length - 1) {
+                    val polymerPair = "$c${polymer[index + 1]}"
+                    incrementCount(tokenCountMap, polymerPair)
                 }
             }
 
-            println(count)
-//            assertEquals(count['B'], 1749)
-//            assertEquals(count['C'], 298)
+            for (i in 1..40) {
+                println(i)
+                val newTokens = emptyList<Pair<String, Long>>().toMutableList()
+                val destroyedTokens = emptyList<Pair<String, Long>>().toMutableList()
+                tokenCountMap.forEach { (token, count) ->
+                    if (count > 0 ) {
+                        val toInsert = rules[token]
+                        toInsert?.apply {
+                            val newToken1 = token.first() + this
+                            val newToken2 = this + token.last()
+                            newTokens.add(Pair(newToken1, count))
+                            newTokens.add(Pair(newToken2, count))
+                            destroyedTokens.add(Pair(token, count))
+                        }
+                    }
+                }
+                println(newTokens)
+                newTokens.forEach { incrementCount(tokenCountMap, it.first, it.second) }
+                destroyedTokens.forEach { decrementCount(tokenCountMap, it.first, it.second) }
+                println(tokenCountMap)
+            }
 
+            println(tokenCountMap)
 
+            val letterCountMap = HashMap<String, Long>()
+            tokenCountMap.forEach { (t, count) ->
+                incrementCount(letterCountMap, t.first().toString(), count)
+                incrementCount(letterCountMap, t.last().toString(), count)
+            }
+
+            //start and end
+            letterCountMap[polymer.first().toString()] = letterCountMap[polymer.first().toString()]!! + 1
+            letterCountMap[polymer.last().toString()] = letterCountMap[polymer.last().toString()]!! + 1
+
+            println(letterCountMap.map { (k, v) -> "$k=${v/2}"  })
+        }
+    }
+
+    private fun incrementCount(
+        map: HashMap<String, Long>,
+        key: String,
+        amount: Long = 1
+    ) {
+        if (map[key] == null) {
+            map[key] = amount
+        } else {
+            map[key] = map[key]!! + amount
+        }
+    }
+    private fun decrementCount(
+        map: HashMap<String, Long>,
+        key: String,
+        amount: Long = 1
+    ) {
+        if (map[key] == null) {
+            throw RuntimeException("Cant decrement key that doesn't exist - $key")
+        } else {
+            map[key] = map[key]!! - amount
         }
     }
 
